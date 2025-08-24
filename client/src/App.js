13b9acc69e9ba282f80a96d957ec62846e25d0f6
hrshk1 <hrsh.110070391@gmail.com>
2025-08-24 21:29:34 +0530

    react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout.js';
 
 function App() {
   return (
     <>
+      <Layout>
       <h1> Ecommerce App </h1>
+      </Layout>
+      {/* when you wrap things inside layout they will be passed as props.children in layout */}
     </>
   );
 }
--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
     <div>
-        <h1>Layout</h1>
+        <Header/>
+        <main style={{minHeight:'80vh'}}>
+        {children}
+        {/* here first layout will run then props.children that contains <h1> Ecommerce App </h1> will run */}
+        </main>
+        <Footer/>
     </div>
   )
 }
